4fa16eb4eaea69bb049d1784889a963250e307d3
Jose Blandon <jose.blandon@ceiba.com.co>
2018-06-26 16:21:28 -0500

    Se agregaron los componentes que se van a utilizar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/common/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ConfigComponent } from './components/config/config.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ConfigComponent
   ],
   imports: [
     BrowserModule
